8c50add928927fce446ca301707f2a128a810190
Nick Shampoe <ShampooShoulders@gmail.com>
2023-03-29 22:31:58 -0400

    Initial Commit

---
+++ RunningConversionCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RunningConversionCalculator
+//
+//  Created by Nicholas Shampoe on 3/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RunningConversionCalculator/RunningConversionCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningConversionCalculatorApp.swift
+//  RunningConversionCalculator
+//
+//  Created by Nicholas Shampoe on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RunningConversionCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
